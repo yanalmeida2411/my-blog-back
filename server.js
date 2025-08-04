2cacc78b9755574709b349fcf206dc0ad124e598
Yan Monteiro <yanalmeida2411@gmail.com>
2025-08-04 11:36:52 -0300

    Mudança no CORS para Deploy

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 3001;
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // sua url do front
+    origin: "https://mynetblog.netlify.app/", // sua url do front
     credentials: true, // habilita cookies
   })
 );
